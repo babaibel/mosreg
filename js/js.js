057cc53d90bc35d344286012916b230848ee4fa8
Babai <babaev-2010@mail.ru>
2017-04-25 10:35:36 +0300

    main page

---
+++ js/js.js
@@ -0,0 +1,168 @@
+var $W = $(window),
+    $D = $(document),
+    $H = $('html'),
+    $B = $('body');
+
+
+$(function() {
+
+    $('.js-map-btn').click(function () {
+    	$(this).toggleClass('active');
+    	if($(this).hasClass('active')){
+    		$(this).text('Свернуть карту');
+    	} else{
+			$(this).text('Развернуть карту');
+    	}
+    	$(this).parent('.map__topline').next('.js-map').slideToggle(200);
+	    return false;
+	});
+
+});
+
+$(function() {
+
+	$(".js-mob_menu").on("click touchstart",function(e){e.preventDefault(),toggleNav()});
+
+	$(".js-close-menu").click(function () {
+		$("#js-canvas").removeClass("layout__canvas_show");
+	});
+
+	function toggleNav(){
+		if( $("#js-canvas").hasClass("layout__canvas_show") ){
+			$("#js-canvas").removeClass("layout__canvas_show");
+		}
+		else{
+			$("#js-canvas").addClass("layout__canvas_show");
+		}
+	}
+
+	$('.js-popup-modal').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#username',
+		showCloseBtn: false,
+		mainClass: 'popup-modal-overlay',
+		callbacks: {
+			open: function() { $('.header__layout.fixed').css('padding-right', swidth + "px"); }, 
+			close: function() { $('.header__layout.fixed').css('padding-right', 0); },
+		}
+	});
+
+	$(document).on('click', '.popup-modal-dismiss', function (e) {
+		e.preventDefault();
+		$.magnificPopup.close();
+	});
+});
+
+$(function () {
+    function setScrolled(){
+        var scroll = $W.scrollTop();
+
+        if(scroll > 90 ){
+            if(!$B.hasClass('_scrolled')) $B.addClass('_scrolled');
+        }else{
+            $B.removeClass('_scrolled');
+        }
+    }
+
+    setScrolled();
+
+    $W.scroll(function(){
+        setScrolled();
+    })
+
+});
+
+$(function() {
+
+	var $services = $('.js-services');
+
+	if(!$services.length) return;
+
+	var $servicesSlider = $services.find('.js-services-slider'),
+		$servicesBtns = $services.find('.js-services-btns > div'),
+		$servicesTabs = $services.find('.js-services-tabs > div'),
+		$servicesSliderBtns = $services.find('.js-services-slider-btns'),
+		$servicesSliderTabs = $services.find('.js-services-slider-tabs > div'),
+		$servicesSliderBtns2 = $services.find('.js-services-slider-btns2'),
+		$servicesSliderTabs2 = $services.find('.js-services-slider-tabs2 > div');
+
+	function servicesTabs(numb) {
+        $servicesTabs.removeClass('active')
+            .eq(numb).addClass('active');
+
+        $servicesBtns.removeClass('active')
+            .eq(numb).addClass('active');
+    }
+
+    function servicesSliderTabs(numb) {
+        $servicesSliderTabs.removeClass('active')
+            .eq(numb).addClass('active');
+
+        $servicesSliderBtns.removeClass('active').parent('div')
+            .eq(numb).children('div').addClass('active');
+    }
+
+     function servicesSliderTabs2(numb) {
+        $servicesSliderTabs2.removeClass('active')
+            .eq(numb).addClass('active');
+
+        $servicesSliderBtns2.removeClass('active').parent('div')
+            .eq(numb).children('div').addClass('active');
+    }
+
+    $servicesBtns.bind('click', function () {
+        var index = $(this).index();
+        servicesTabs(index);
+        $servicesSlider.slick('setPosition');
+    });
+
+    $servicesSliderBtns.bind('click', function () {
+        var index = $(this).parent('div').index();
+        servicesSliderTabs(index);
+    });
+
+    $servicesSliderBtns2.bind('click', function () {
+        var index = $(this).parent('div').index();
+        servicesSliderTabs2(index);
+    });
+
+
+	$servicesSlider.slick({
+	  infinite: false,
+	  arrows: false,
+	  slidesToShow: 6,
+	  slidesToScroll: 3,
+	  dots: true,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 5
+	      }
+	    },
+	    {
+	      breakpoint: 860,
+	      settings: {
+	        slidesToShow: 4
+	      }
+	    },
+	    {
+	      breakpoint: 780,
+	      settings: {
+	        slidesToShow: 3
+	      }
+	    },
+	    {
+	      breakpoint: 560,
+	      settings: {
+	        centerMode: true,
+	        variableWidth: true,
+	        slidesToShow: 1
+	      }
+	    }
+	  ]
+	});
+
+});
+
